ae1b08651ae3252954bf4bdaade911d9d1f2e81f
Laura Gwilliams <laura.gwilliams@ucsf.edu>
2021-02-24 17:02:08 -0800

    change trial to csv

--- sequence/sequence-1_lastrun.py
+++ sequence/sequence-1_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.9),
-    on Mon Feb  1 10:03:59 2021
+    on Wed Feb 24 16:45:23 2021
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -57,7 +57,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['Subject ID'], expNam
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='/Users/lauragwilliams/Dropbox (UCSF Department of Neurological Surgery)/lg/lexical_access/new_task/sequence/sequence-1_lastrun.py',
+    originPath='/Users/lauragwilliams/Documents/projects/barakeet/phoneme_seequence/sequence/sequence-1_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
